28c7f3d9441183bf3dfdcc6e32ae437da2826f6c
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-09-26 18:03:46 -0400

    fix gitignore

--- JS/01-intro-to-js/Activities/sec b/08-built-ins/solved/script.js
+++
@@ -1,9 +0,0 @@
-console.log('Your JS file is linked!');
-
-// Print a random integer (whole number) between 50(inclusive) and 100(exclusive) to the console
-// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-//Write your code below this line:
-
-let randomNumber = Math.floor(Math.random() * 50);
-console.log(randomNumber);
