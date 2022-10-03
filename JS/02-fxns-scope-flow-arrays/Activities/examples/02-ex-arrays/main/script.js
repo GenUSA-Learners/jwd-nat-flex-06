f135d4525be7c58ff22ca0067f510f36d1591c72
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-10-03 17:46:29 -0400

    Add example

--- JS/02-fxns-scope-flow-arrays/Activities/examples/02-ex-arrays/main/script.js
+++ JS/02-fxns-scope-flow-arrays/Activities/examples/02-ex-arrays/main/script.js
@@ -1,5 +1,7 @@
+// empty arrays don't return undefined
 
-
+const groceryList = [];
+console.log(groceryList);
 
 
 // If your usecase only needs mutation, you can go for const.. if you need reassigning then go for let.
@@ -10,14 +12,22 @@ let condiments = ['Ketchup', 'Soy Sauce', 'Sriracha'];
 
 // Mutation possible
 condiments[0] = 'Mayo';
-console.log(condiments);//=> [ 'Mayo', 'Soy Sauce', 'Sriracha' ]
+//console.log(condiments);//=> [ 'Mayo', 'Soy Sauce', 'Sriracha' ]
 
 // Re-assigning possible
 condiments = ['Mayo'];
-console.log(condiments); //=> [ 'Mayo' ]
+//console.log(condiments); //=> [ 'Mayo' ]
 
 // Re-declaring not possible
 //let condiments = [] //=> SyntaxError: Identifier 'condiments' has already been declared
+
+
+
+
+
+
+
+
 
 
 // CONST
@@ -26,9 +36,9 @@ const utensils = ['Fork', 'Chopsticks', 'Spork'];
 
 // Mutation Possible
 utensils[2] = 'Spoon'
-console.log(utensils); //=> [ 'Fork', 'Chopsticks', 'Spoon' ]
+//console.log(utensils); //=> [ 'Fork', 'Chopsticks', 'Spoon' ]
 utensils.length = 0
-console.log(utensils); //=> [ ]
+//console.log(utensils); //=> [ ]
 
 // Re-assigning not possible
 //utensils = ['Spoon']; //=> TypeError: Assignment to constant variable.
